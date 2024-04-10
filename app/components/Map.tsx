730b4a76bc55c93c3e1276dae99cbd9dc3e649f4
Sebastian Gallardo <sebastian.gallardo@nan-labs.com>
2024-04-10 10:15:49 -0300

    add controls to map

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React from "react";
-import { Map as Mapbox } from "react-map-gl";
+import {
+  FullscreenControl,
+  Map as Mapbox,
+  NavigationControl,
+  ScaleControl,
+} from "react-map-gl";
 
 const accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
@@ -13,10 +18,16 @@ const Map: React.FC = () => {
         longitude: -122.4,
         latitude: 37.8,
         zoom: 14,
+        bearing: 0,
+        pitch: 0,
       }}
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    >
+      <FullscreenControl position="top-left" />
+      <NavigationControl position="top-left" />
+      <ScaleControl />
+    </Mapbox>
   );
 };
 
